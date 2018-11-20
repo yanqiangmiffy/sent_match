36cb56d4f546e432d00479d3bbc84e621cf197e0
yanqiangmiffy <1185918903@qq.com>
2018-11-20 14:52:31 +0800

    更新

--- eda.py
+++ eda.py
@@ -15,45 +15,45 @@ question = pd.read_csv('input/question_id.csv')
 train = pd.read_csv('input/train.csv')
 test = pd.read_csv('input/test.csv')
 
-# train.rename(columns={'qid1':'q1','qid2':'q2'},inplace=True)
-# test.rename(columns={'qid1':'q1','qid2':'q2'},inplace=True)
-# question.rename(columns={'wid':'words','cid':'chars'},inplace=True)
+train.rename(columns={'qid1':'q1','qid2':'q2'},inplace=True)
+test.rename(columns={'qid1':'q1','qid2':'q2'},inplace=True)
+question.rename(columns={'wid':'words','cid':'chars'},inplace=True)
 #
-# train.to_csv('train.csv',index=False)
-# test.to_csv('test.csv',index=False)
-# question.to_csv('question.csv',index=False)
+train[['label','q1','q2']].to_csv('train.csv',index=False)
+test[['q1','q2']].to_csv('test.csv',index=False)
+question.to_csv('question.csv',index=False)
 
-print(train['label'].value_counts())
-print(train['qid1'].value_counts()[:10])  # 问题有重复的
-
-# 句子长度统计
-question['word_len'] = question['wid'].apply(lambda x: len(x.split(' ')))
-question['char_len'] = question['cid'].apply(lambda x: len(x.split(' ')))
-question['word_len'].value_counts()[:40].plot(kind='bar')
-plt.show()
-print(question.describe())
-
-# 词频统计
-words = []
-for word_sent in question['wid']:
-    words.extend(word_sent.split(' '))
-print(Counter(words), '\n', len(Counter(words)))
-
-words = []
-for word_sent in question['cid']:
-    words.extend(word_sent.split(' '))
-print(Counter(words), '\n', len(Counter(words)))
-
-import numpy as np
-a = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
-b = np.array([[4, 5, 6], [4, 5, 6], [4, 5, 6]])
-c = np.stack((a, b), axis=0)
-print(c[:,:,:])
-print("结果一样")
-print(c)
-print("---")
-
-print(c[:1,:,:])
-
-
-
+# print(train['label'].value_counts())
+# print(train['qid1'].value_counts()[:10])  # 问题有重复的
+#
+# # 句子长度统计
+# question['word_len'] = question['wid'].apply(lambda x: len(x.split(' ')))
+# question['char_len'] = question['cid'].apply(lambda x: len(x.split(' ')))
+# question['word_len'].value_counts()[:40].plot(kind='bar')
+# plt.show()
+# print(question.describe())
+#
+# # 词频统计
+# words = []
+# for word_sent in question['wid']:
+#     words.extend(word_sent.split(' '))
+# print(Counter(words), '\n', len(Counter(words)))
+#
+# words = []
+# for word_sent in question['cid']:
+#     words.extend(word_sent.split(' '))
+# print(Counter(words), '\n', len(Counter(words)))
+#
+# import numpy as np
+# a = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+# b = np.array([[4, 5, 6], [4, 5, 6], [4, 5, 6]])
+# c = np.stack((a, b), axis=0)
+# print(c[:,:,:])
+# print("结果一样")
+# print(c)
+# print("---")
+#
+# print(c[:1,:,:])
+#
+#
+#
